679d48d06ae3c505c700e013f68206d34f653ccc
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2023-04-19 02:17:33 +0200

    Add palette colors

---
+++ lib/resources/palette_colors.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class PaletteColors {
+  static const Color primary = Color(0xff28d0b5);
+  static const Color secondary = Color(0xff9df0e5);
+  static const Color active = Color(0xfff3b4fa);
+  static const Color background = Color(0xfff9ddfa);
+  static const Color text = Color(0xff313047);
+}
