1618c1beec12bcb53bcc3deda98d1e70bb0f3f36
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2023-04-20 17:55:58 +0200

    Create this two pages to try the navigation

---
+++ lib/features/home_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('Home' * 1000),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/features/sing_up/login_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              Text('Login' * 1000),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
