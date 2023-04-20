9e7b20169e0e6f668fe14d95047007baad0a6c6f
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2023-04-20 15:07:31 +0200

    Create palette for light and dark mode also add material colors palette

--- lib/resources/palette_colors.dart
+++ lib/resources/palette_colors.dart
@@ -4,6 +4,27 @@ class PaletteColors {
   static const Color primary = Color(0xff28d0b5);
   static const Color secondary = Color(0xff9df0e5);
   static const Color active = Color(0xfff3b4fa);
-  static const Color background = Color(0xfff9ddfa);
+}
+
+class PaletteColorsLight extends PaletteColors {
+  static const Color background = Color(0xfff3edf5);
   static const Color text = Color(0xff313047);
+}
+
+class PaletteColorsDark extends PaletteColors {
+  static const Color background = Color(0xff313047);
+  static const Color text = Color(0xfff3edf5);
+}
+
+class PaletteMaterialColors {
+  static const MaterialColor primary = MaterialColor(0xff28d0b5, {
+    50: Color.fromRGBO(40, 208, 181, .1),
+    100: Color.fromRGBO(40, 208, 181, .2),
+    200: Color.fromRGBO(40, 208, 181, .3),
+    300: Color.fromRGBO(40, 208, 181, .4),
+    400: Color.fromRGBO(40, 208, 181, .5),
+    500: Color.fromRGBO(40, 208, 181, .6),
+    600: Color.fromRGBO(40, 208, 181, .7),
+    700: Color.fromRGBO(40, 208, 181, .8),
+  });
 }
