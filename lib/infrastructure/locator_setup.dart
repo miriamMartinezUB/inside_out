e601041716e3a91fc78c7ae76d433e2905c4956e
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2023-04-20 16:10:31 +0200

    Add new services to set up

--- lib/infrastructure/locator_setup.dart
+++ lib/infrastructure/locator_setup.dart
@@ -1,12 +1,23 @@
 import 'package:get_it/get_it.dart';
 import 'package:inside_out/infrastructure/language_service.dart';
+import 'package:inside_out/infrastructure/locale_storage_service.dart';
+import 'package:inside_out/infrastructure/theme_service.dart';
 
 GetIt locator = GetIt.instance;
 
+LocaleStorageService _localeStorageService = LocaleStorageService();
+LanguageService _languageService = LanguageService();
+ThemeService _themeService = ThemeService(_localeStorageService);
+
 void setupLocator() {
-  locator.registerLazySingleton(() => LanguageService());
+  locator.registerLazySingleton(() => _localeStorageService);
+  locator.registerLazySingleton(() => _languageService);
+  locator.registerLazySingleton(() => _themeService);
 }
 
+/// Warning: the order is important, to keep the dependencies right
 Future<void> initializeConfigurationServices() async {
-  await locator<LanguageService>().initDelegate();
+  await _localeStorageService.init();
+  await _languageService.initDelegate();
+  _themeService.init();
 }
