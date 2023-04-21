f39e532e9908d3842e8b88cb0c1e1fbd13d5e9ef
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2023-04-21 09:41:06 +0200

    Adapt palette colors light and dark to be able to access from palette colors object

--- lib/resources/palette_colors.dart
+++ lib/resources/palette_colors.dart
@@ -4,16 +4,25 @@ class PaletteColors {
   Color primary = const Color(0xff28d0b5);
   Color secondary = const Color(0xff9df0e5);
   Color active = const Color(0xfff3b4fa);
+  late Color background;
+  late Color text;
+  late Color textSubtitle;
 }
 
 class PaletteColorsLight extends PaletteColors {
-  Color background = const Color(0xfff3edf5);
-  Color text = const Color(0xff313047);
+  PaletteColorsLight() {
+    background = const Color(0xfff3edf5);
+    text = const Color(0xff313047);
+    textSubtitle = const Color.fromRGBO(49, 48, 71, .6);
+  }
 }
 
 class PaletteColorsDark extends PaletteColors {
-  Color background = const Color(0xff313047);
-  Color text = const Color(0xfff3edf5);
+  PaletteColorsDark() {
+    background = const Color(0xff313047);
+    text = const Color(0xfff3edf5);
+    textSubtitle = const Color.fromRGBO(243, 237, 245, .6);
+  }
 }
 
 class PaletteMaterialColors {
