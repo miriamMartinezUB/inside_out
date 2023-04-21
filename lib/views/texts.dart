a41f504e2714c1cea6cc63a6a39e283542d495cc
miriamMartinezUB <31921873+miriamMartinezUB@users.noreply.github.com>
2023-04-21 09:41:52 +0200

    Add dependency and create a component to manage all texts of app

---
+++ lib/views/texts.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_stylized_text/flutter_stylized_text.dart';
+import 'package:inside_out/infrastructure/theme_service.dart';
+import 'package:inside_out/resources/dimens.dart';
+import 'package:inside_out/resources/palette_colors.dart';
+import 'package:provider/provider.dart';
+
+enum TextTypes {
+  title,
+  subtitle,
+  body,
+  smallBody,
+  tinyBody,
+}
+
+class AppText extends StatelessWidget {
+  final String text;
+  final Color? color;
+  final TextTypes type;
+  final TextAlign align;
+
+  const AppText(
+    this.text, {
+    required this.type,
+    required this.align,
+    this.color,
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    PaletteColors paletteColors = Provider.of<ThemeService>(context).paletteColors;
+    return FlutterStylizedText(
+      text: text,
+      fontSize: _getFontSize(type: type),
+      fontWeight: _getFontWeight(type: type),
+      textAlign: align,
+      stylizedTextColor: paletteColors.primary,
+      color: color ?? _getColor(paletteColors, type: type),
+    );
+  }
+}
+
+TextStyle getTextStyle({
+  required PaletteColors paletteColors,
+  TextTypes type = TextTypes.body,
+  Color? color,
+}) {
+  return TextStyle(
+    color: color ?? _getColor(paletteColors, type: type),
+    fontWeight: _getFontWeight(type: type),
+    fontSize: _getFontSize(type: type),
+  );
+}
+
+Color _getColor(PaletteColors paletteColors, {TextTypes type = TextTypes.body}) {
+  if (type.name.toLowerCase().contains('subtitle')) {
+    return paletteColors.textSubtitle;
+  }
+  return paletteColors.text;
+}
+
+double _getFontSize({TextTypes type = TextTypes.body}) {
+  if (type.name.toLowerCase().contains('subtitle')) {
+    return Dimens.textSizeSubtitle;
+  } else if (type.name.toLowerCase().contains('title')) {
+    return Dimens.textSizeTitle;
+  } else if (type.name.toLowerCase().contains('small')) {
+    return Dimens.textSizeBodySmall;
+  } else if (type.name.toLowerCase().contains('tiny')) {
+    return Dimens.textSizeBodyTiny;
+  }
+  return Dimens.textSizeBody;
+}
+
+FontWeight _getFontWeight({TextTypes type = TextTypes.body}) {
+  if (type.name.toLowerCase().contains('light')) {
+    return Dimens.fontWeightLight;
+  } else if (type.name.toLowerCase().contains('medium')) {
+    return Dimens.fontWeightMedium;
+  } else if (type.name.toLowerCase().contains('bold')) {
+    return Dimens.fontWeightBold;
+  }
+  return Dimens.fontWeightRegular;
+}
