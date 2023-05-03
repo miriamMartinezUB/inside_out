69f6bfc3dc778e6ce40577beb53d13892d0d1bbb
Miriam Martinez <miriam.martinez.mc@gmail.com>
2023-05-04 01:33:04 +0200

    Fix on tap

--- lib/views/buttons/app_outlined_button.dart
+++ lib/views/buttons/app_outlined_button.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_translate/flutter_translate.dart';
 import 'package:inside_out/infrastructure/theme_service.dart';
 import 'package:inside_out/resources/dimens.dart';
 import 'package:inside_out/resources/palette_colors.dart';
@@ -22,29 +23,30 @@ class AppOutlinedButton extends StatelessWidget {
     final PaletteColors paletteColors = Provider.of<ThemeService>(context).paletteColors;
 
     return OutlinedButton(
-        style: ButtonStyle(
-          overlayColor: MaterialStateProperty.all(paletteColors.background),
-          foregroundColor: MaterialStateProperty.all(paletteColors.primary),
-          side: MaterialStateProperty.all(
-            BorderSide(color: paletteColors.primary),
-          ),
-          shape: MaterialStateProperty.all(
-            RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(Dimens.radiusMedium),
-            ),
+      style: ButtonStyle(
+        overlayColor: MaterialStateProperty.all(paletteColors.background),
+        foregroundColor: MaterialStateProperty.all(paletteColors.primary),
+        side: MaterialStateProperty.all(
+          BorderSide(color: paletteColors.primary),
+        ),
+        shape: MaterialStateProperty.all(
+          RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(Dimens.radiusMedium),
           ),
         ),
-        onPressed: () {},
-        child: SizedBox(
-          width: double.infinity,
-          child: Padding(
-            padding: const EdgeInsets.all(Dimens.paddingLarge),
-            child: AppText(
-              text,
-              type: TextTypes.body,
-              align: TextAlign.center,
-            ),
+      ),
+      onPressed: onTap,
+      child: SizedBox(
+        width: double.infinity,
+        child: Padding(
+          padding: const EdgeInsets.all(Dimens.paddingLarge),
+          child: AppText(
+            shouldTranslate ? translate(text) : text,
+            type: TextTypes.body,
+            align: TextAlign.center,
           ),
-        ));
+        ),
+      ),
+    );
   }
 }
