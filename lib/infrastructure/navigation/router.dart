0f16ee5ba1a482dacc988c27f4c8ebe5f5003d86
Miriam Martinez <miriam.martinez.mc@gmail.com>
2023-05-04 01:37:12 +0200

    Add missing routes

--- lib/infrastructure/navigation/router.dart
+++ lib/infrastructure/navigation/router.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
-import 'package:inside_out/features/home_page.dart';
-import 'package:inside_out/features/sing_up/login_page.dart';
+import 'package:inside_out/features/common/main_flow/main_flow_page.dart';
+import 'package:inside_out/features/history/history_page.dart';
+import 'package:inside_out/features/information/information_page.dart';
+import 'package:inside_out/features/results/results_page.dart';
+import 'package:inside_out/features/sign_up/forgot_password_page.dart';
+import 'package:inside_out/features/sign_up/login_page.dart';
+import 'package:inside_out/features/sign_up/sign_up_page.dart';
+import 'package:inside_out/features/welcome_page.dart';
 import 'package:inside_out/resources/routes.dart';
 
 class InsideOutRouter {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case Routes.initialRoute:
+      case Routes.login:
         return MaterialPageRoute(builder: (context) => const LoginPage());
+      case Routes.signUp:
+        return MaterialPageRoute(builder: (context) => const SignUpPage());
+      case Routes.forgotPassword:
+        return MaterialPageRoute(builder: (context) => const ForgotPasswordPage());
       case Routes.home:
-        return MaterialPageRoute(builder: (context) => const HomePage());
+        return MaterialPageRoute(builder: (context) => const MainFlowPage());
+      case Routes.information:
+        return MaterialPageRoute(builder: (context) => const InformationPage());
+      case Routes.results:
+        return MaterialPageRoute(builder: (context) => const ResultsPage());
+      case Routes.history:
+        return MaterialPageRoute(builder: (context) => const HistoryPage());
+      case Routes.initialRoute:
       default:
-        return MaterialPageRoute(builder: (context) => const LoginPage());
+        return MaterialPageRoute(builder: (context) => const WelcomePage());
     }
   }
 }
--- lib/resources/routes.dart
+++ lib/resources/routes.dart
@@ -1,6 +1,10 @@
 class Routes {
   static const initialRoute = '/';
   static const login = '/login';
-  static const register = '/register';
+  static const forgotPassword = '/forgot_password';
+  static const signUp = '/sign_up';
   static const home = '/home';
+  static const information = '/information';
+  static const results = '/results';
+  static const history = '/history';
 }
