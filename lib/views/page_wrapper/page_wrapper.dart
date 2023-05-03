f4cda62ecb0c0e2d5c6dbc062166599fd1006b56
Miriam Martinez <miriam.martinez.mc@gmail.com>
2023-05-04 01:31:42 +0200

    Build Page wrapper to make easy build the pages the same way in web or in mobile

---
+++ lib/views/page_wrapper/page_wrapper.dart
@@ -0,0 +1,2 @@
+export 'package:inside_out/views/page_wrapper/page_wrapper_mobile.dart'
+    if (dart.library.html) 'package:inside_out/views/page_wrapper/page_wrapper_web.dart';
---
+++ lib/views/page_wrapper/page_wrapper_mobile.dart
@@ -0,0 +1,53 @@
+import 'package:flutter/material.dart';
+import 'package:inside_out/infrastructure/theme_service.dart';
+import 'package:inside_out/resources/palette_colors.dart';
+import 'package:inside_out/views/page_wrapper/inside_out_app_bar.dart';
+import 'package:provider/provider.dart';
+
+class PageWrapper extends StatelessWidget {
+  final Widget body;
+  final bool isMainPage;
+  final bool showAppBar;
+  final String? appBarName;
+  final Function? onPop;
+
+  PageWrapper({
+    Key? key,
+    required this.body,
+    this.isMainPage = false,
+    this.showAppBar = true,
+    this.appBarName,
+    this.onPop,
+  }) : super(key: key) {
+    if (showAppBar && appBarName == null) {
+      throw FlutterError('If showAppBar is true appBarName attribute is required');
+    }
+    if (showAppBar && !isMainPage) {
+      throw FlutterError('showAppBar and !isMainPage is not compatible');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final PaletteColors paletteColors = Provider.of<ThemeService>(context).paletteColors;
+    return WillPopScope(
+      onWillPop: () async {
+        onPop?.call();
+        return !isMainPage;
+      },
+      child: Scaffold(
+        backgroundColor: paletteColors.background,
+        appBar: showAppBar
+            ? InsideOutAppBar(
+                isMainPage: isMainPage,
+                onPop: onPop,
+                appBarName: appBarName!,
+              )
+            : null,
+        body: SafeArea(
+          child: body,
+        ),
+      ),
+    );
+  }
+}
