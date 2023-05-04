802d906b617420ae0e04c5bfdfc695a504371aa2
Miriam Martinez <miriam.martinez.mc@gmail.com>
2023-05-04 18:48:44 +0200

    Add attributes in the widget to make it reusable

--- lib/features/information/views/card_information.dart
+++ lib/features/information/views/card_information.dart
@@ -2,46 +2,60 @@ import 'package:flutter/material.dart';
 import 'package:inside_out/infrastructure/theme_service.dart';
 import 'package:inside_out/resources/dimens.dart';
 import 'package:inside_out/resources/palette_colors.dart';
+import 'package:inside_out/views/texts.dart';
 import 'package:provider/provider.dart';
 
 Radius _radius = const Radius.circular(Dimens.radiusLarge);
 
 class CardInformation extends StatelessWidget {
-  const CardInformation({Key? key}) : super(key: key);
+  final String title;
+  final Function()? onTap;
+  const CardInformation({
+    Key? key,
+    required this.title,
+    this.onTap,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     final PaletteColors paletteColors = Provider.of<ThemeService>(context).paletteColors;
 
     return InkWell(
-      child: Padding(
-        padding: const EdgeInsets.all(Dimens.paddingLarge),
-        child: Column(
-          children: [
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    topLeft: _radius,
-                    topRight: _radius,
-                  ),
-                  color: paletteColors.card,
+      onTap: onTap,
+      child: Column(
+        children: [
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(Dimens.paddingLarge),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  topLeft: _radius,
+                  topRight: _radius,
                 ),
+                color: paletteColors.card,
               ),
             ),
-            Expanded(
-              child: Container(
-                decoration: BoxDecoration(
-                  borderRadius: BorderRadius.only(
-                    bottomLeft: _radius,
-                    bottomRight: _radius,
-                  ),
-                  color: paletteColors.primary,
+          ),
+          Expanded(
+            child: Container(
+              padding: const EdgeInsets.all(Dimens.paddingMedium),
+              width: double.infinity,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.only(
+                  bottomLeft: _radius,
+                  bottomRight: _radius,
                 ),
+                color: paletteColors.primary,
+              ),
+              child: AppText(
+                title,
+                color: paletteColors.textButton,
+                type: TextTypes.smallBodyMedium,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
